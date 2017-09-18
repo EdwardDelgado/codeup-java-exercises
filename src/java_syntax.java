2566deeb66577358f87b664455bd982577e047c5
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-18 16:30:10 -0500

    Completed Exercise

---
+++ src/java_syntax.java
@@ -0,0 +1,44 @@
+public class java_syntax {
+    public static void main(String[] args) {
+
+        int myFavoriteNumber = 54;
+        System.out.println("My favorite number is " + myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x2 = 5;
+        System.out.println(++x2);
+        System.out.println(x2);
+
+//        String class = "Edward";    class is a reserved word (it won't work)
+//        String className = "Redwood"; This could work
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+        int y = 4;
+        y *= x;
+        System.out.println(y);
+
+//        x /= y;
+//        y -= x;
+
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
+
+
+
+    }
+}
