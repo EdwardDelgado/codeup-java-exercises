1c46f471536318cf10bdc44a028f8ed0fbe50a7a
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-18 10:00:25 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+//        psvm
+//    Entry point
+    public static void main(String[] args) {
+//        sout
+        System.out.print("Hello World!");
+        System.out.println("Hello Redwood!");
+    }
+}
