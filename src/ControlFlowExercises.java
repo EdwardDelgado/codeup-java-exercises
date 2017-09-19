4ac8a6e703a37426aa55844ec2ef8fd2917b8da0
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-19 15:10:45 -0500

    Control Flow Exercise 1B-1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String [] args) {
-    int i = 5;
-        while(i <= 15){
-            System.out.println(i);
-            i++;
+//    int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+
+      int count = 0;
+        while(count <= 100){
+            System.out.println(count);
+            count = count + 2;
+        }
         }
     }
-}
+//}
