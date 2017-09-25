ec8b92c56472e94c6f63b45d31b961cd1bc33144
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-25 15:32:02 -0500

    Person Object Exercise, completed #1

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+
+    // returns the person's name
+    public String getName(){
+        return name;
+    }
+    // changes the name property to the passed value
+    public void setName(String name){
+        this.name = name;
+    }
+    // prints a message to the console using the person's name
+    public void sayHello(){
+        System.out.println("Howdy from " + this.name);
+    }
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Tom");
+        person.sayHello();
+    }
+}
