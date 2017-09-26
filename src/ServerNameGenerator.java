fa138865de92b457a001abe2e9414f58d26a723a
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-26 15:56:31 -0500

    Begin Java Arrays Exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,12 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+        String[] adjectives = {"Scared", "Happy", "Emotional", "Pregnant", "Senile", "Hungry", "Competitive", "Dangerous", "Aggressive", "Pure"};
+        adjectives = new String[10];
+
+        String[] nouns = {"Art", "Government", "Literature", "Software", "Knowledge", "Science", "Library", "Investment", "Society", "Analysis"};
+        nouns = new String[10];
+
+    }
+
+}
