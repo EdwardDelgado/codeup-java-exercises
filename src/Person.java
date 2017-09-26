2694985c5257d572fb7f1cc33afc66e1f447d3cf
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-26 15:10:09 -0500

    Complete Person Objects Exercise

--- src/Person.java
+++ src/Person.java
@@ -7,40 +7,39 @@ public class Person {
 
     // returns the person's name
     public String getName(){
+
         return name;
     }
+
     // changes the name property to the passed value
     public void setName(String name){
+
         this.name = name;
     }
+
     // prints a message to the console using the person's name
     public void sayHello(){
-        System.out.println("Howdy " + this.name);
+
+        System.out.println("Howdy " + this.name + ", Nice to see You!");
     }
 
     public static void main(String[] args) {
-//        Person person = new Person("Gunnar");
+//        Person person = new Person("Tom");
 
-//        person.setName("Graham");
+//        person.setName("Dan");
 
 //        person.sayHello();
 
 //        System.out.println(person.getName());
 
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
 
-//        System.out.println(person1.getName().equals(person2.getName())); //True
-
-//        System.out.println(person1 == person2); //False
-
-        Person person1 = new Person("John");
+        Person person1 = new Person("Gunnar");
         Person person2 = person1;
 
         System.out.println(person1.getName());
         System.out.println(person2.getName());
 
-        person2.setName("Jane");
+        person2.setName("Graham");
 
         System.out.println(person1.getName());
         System.out.println(person2.getName());
--- src/util/Input.java
+++ src/util/Input.java
@@ -7,7 +7,6 @@ public class Input {
 
     public Input() {
 
-
         scanner = new Scanner(System.in);
     }
 
@@ -17,26 +16,52 @@ public class Input {
     }
 
     public boolean yesNo(){
-        System.out.print("y / n ?");
         String userInput = this.scanner.nextLine();
         return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
     }
+
     public int getInt(){
         if (this.scanner.hasNextInt()){
         return this.scanner.nextInt();
     } else {
-            System.out.println("Invalid Input!");
+            System.out.println("That's not an integer! Try again.");
+            scanner.nextLine();
             return getInt();
+            }
 
+                }
+
+        public int getInt(int min, int max) {
+        int userInput = getInt();
+
+                if (userInput < min || userInput > max) {
+            System.out.println(userInput + " is not between " + min + " and " + max);
+            return getInt(min, max);
+                    }
+
+                    return userInput;
         }
-}
 
-    public int getInt(int min, int max) {
-    int userInput = getInt();
+         public double getDouble() {
+            if (this.scanner.hasNextDouble()) {
+            return this.scanner.nextDouble();
+            } else {
+            System.out.println("That's not an double! Try again.");
+            scanner.nextLine();
+            return getDouble();
+            }
+        }
 
-    if (userInput < min || userInput > max){
+         public double getDouble(double min, double max) {
+         double userInput = getDouble();
 
-    }
+                if (userInput < min || userInput > max) {
+            System.out.println(userInput + " is not between " + min + " and " + max);
+            return getDouble(min, max);
+            }
 
-    }
-}
+                return userInput;
+        }
+
+         }
+
