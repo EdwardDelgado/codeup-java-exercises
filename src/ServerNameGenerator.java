17fb82efd3084aa2710995096a6801db268937fe
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-27 10:18:14 -0500

    Java Arrays Exercise #1

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,19 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
+
+         private static String[] adjectives = new String[]{"Scared", "Happy", "Emotional", "Pregnant", "Senile", "Hungry", "Competitive", "Dangerous", "Aggressive", "Pure"};
+
+         private static String[] nouns = {"Art", "Government", "Literature", "Software", "Knowledge", "Science", "Library", "Investment", "Society", "Analysis"};
+
+    public static String getRandom(String[] server) {
+        Random random = new Random();
+        int rand = random.nextInt(server.length);
+        return server[rand];
+    }
     public static void main(String[] args) {
-
-        String[] adjectives = {"Scared", "Happy", "Emotional", "Pregnant", "Senile", "Hungry", "Competitive", "Dangerous", "Aggressive", "Pure"};
-        adjectives = new String[10];
-
-        String[] nouns = {"Art", "Government", "Literature", "Software", "Knowledge", "Science", "Library", "Investment", "Society", "Analysis"};
-        nouns = new String[10];
+        System.out.println("Here is your server name: \n "
+                + getRandom(adjectives) + " - " + getRandom(nouns));
 
     }
 
