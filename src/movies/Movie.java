1e939b270f192122ee8a6fde340b87a6f2f76734
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-27 15:01:00 -0500

    Java Arrays Exercise working on #2

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -6,8 +6,16 @@ public class Movie {
 
     private String category;
 
-    public Movie(String name){
-
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public String getCategory(){
+        return category;
+    }
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,4 +1,10 @@
 package movies;
 
 public class MoviesApplication {
+    public static void main(String[] args) {
+        Movie[] movies = MoviesArray.findAll();
+        for (Movie movie : movies) {
+            System.out.println(movie.getName() + " " + movie.getCategory());
+        }
+    }
 }
