e1371bef9b0fa9ba881c30ebebce601c8f7ccbcb
Edward Delgado Jr <edward.delgado@icloud.com>
2017-09-29 13:59:06 -0500

    Complete Interfaces & Abstract Classes Exercise, as well as High Low Exercise.

--- src/Shapes/Quadrilateral.java
+++ src/Shapes/Quadrilateral.java
@@ -1,9 +1,20 @@
-package Shapes;
+package shapes;
 
-class Quadrilateral extends Shape{
+public abstract class Quadrilateral extends Shape {
+    protected double length;
+    protected double width;
 
-}
+         public Quadrilateral(double length, double width) {
+             this.length = length;
+             this.width = width;
+        }
 
-class Quadrilateral implements Measurable{
+         public double getLength() {
+             return length;
+        }
 
-}
+         public double getWidth() {
+            return width;
+        }
+ }
+
--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,6 +1,32 @@
 package Shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 
+public Square(double side){
+    super(side, side);
+}
 
+    @Override
+    public void setLength(double side){
+        this.length = side;
+        this.width = side;
+}
+
+    @Override
+    public void setWidth(double side){
+        this.length = side;
+        this.width = side;
+}
+
+    @Override
+    public double getPerimeter(){
+        double side = this.length;
+        return side * 4;
+}
+
+    @Override
+    public double getArea(){
+        double side = this.length;
+        return Math.pow(side, 2);
+    }
 }
